6842ee208d0c47c628543e5e4e687b1c1f301fc2
Eleanor <e.e.j@live.co.uk>
2020-01-20 14:29:09 +0000

    project completed for now, refactored code and wrote a few more comments

--- minesweeper.js
+++ minesweeper.js
@@ -1,27 +1,32 @@
 ((d) => {
 
-    let width = 10;
-    let height = 10;
-    let number = 3;
+    // changeable parameters: width of board, height of board and number of mines
+    let width = 15;
+    let height = 15;
+    let number = 15;
 
     let main = d.getElementById('grid-container');
 
     main.style.width = width * 50 + 'px';
 
-
     let createGrid = (width, height) => { 
-        
-        for(let i = 1; i <= (width * height); i += 1 ){
 
+        let fragment = document.createDocumentFragment();
+        for(let i = 1; i <= (width * height); i += 1 ){
             let tile = d.createElement("div");
             tile.classList.add("tile");
             tile.setAttribute("id", `${i}`);
             tile.textContent = "";
-            main.append(tile);
+            fragment.append(tile);
         }
+
+        main.append(fragment);
     }
 
+    createGrid(width, height);
+
     let addMines = (number) => {
+        // using a Set here rather than a standard array as it only accepts unique values. 
         let mines = new Set();
 
         while (mines.size < number){ 
@@ -30,8 +35,6 @@
 
         mines = Array.from(mines).sort();
 
-        console.log(mines);
-
         mines.forEach(current => {
             let tile = d.getElementById(current);
             tile.classList.add("mine-location");
@@ -39,7 +42,9 @@
         });
     }
 
-    let addNumberMines = () => {
+    addMines(number);
+
+    let plotSurroundingTiles = () => {
         let allTiles = Array.from(d.getElementsByClassName('tile'));
 
         let tilesWithMines = allTiles.filter(current => current.textContent === "X").map(current => current.id);
@@ -47,8 +52,6 @@
         let tilesWithoutMines = allTiles.filter(current => current.textContent !== "X");
 
         tilesWithoutMines.forEach(current => {
-            let value = "*";
-            // these are the tiles with a mine on them
             let number = +current.id;
 
             let rowNo = Math.ceil(number / width);
@@ -56,53 +59,50 @@
             // column nunber calculated with modulus: if column is on right hand side then it will be the same as width instead. 
             let colNo = number % width === 0 ? width : number % width;
 
-            // working out values for each position relative to the chosen tile. 
+            // working out id values for each position relative to the chosen tile. 
             // ternaries to make sure that there isn't a value given for those tiles on the edges of the board 
 
-            let left = colNo !== 1 ? (number - 1) : null;
+            //immediate surrounding tiles...
+            let left = (colNo - 1) >= 1 ? (number - 1) : null;
 
-            let right = colNo !== width ? (number + 1) : null;
+            let right = (colNo + 1) <= width ? (number + 1) : null;
 
             let above = rowNo !== 1 ? (number - width) : null;
             
-            let below = number + width;
+            let below = rowNo !== width ? (number + width) : null;
 
-            // **** 
-            let topLeft = left - width;
+            // diagonal surrounding tiles
+            let topLeft = left - width >= 1 ? left - width : null;
 
-            let topRight = right - width;
+            let topRight = right - width >= 1 ? right - width : null;
   
             let bottomLeft = left + width;
             
             let bottomRight = right + width;
            
-            let surrounding = [topLeft, above, topRight, left, right, bottomLeft, below, bottomRight];
-
-            let surroundingTiles = surrounding.filter(current => current );
-
+            let surroundingTiles = [topLeft, above, topRight, left, right, bottomLeft, below, bottomRight]
+            
+            surroundingTiles.filter(current => current !== null );
 
             // iterate through all the surrounding tiles for this particular tile. 
             let mineMatches = surroundingTiles.map(current => {
-                // only return the mines where the surrounding tile matches one of the tiles with a mine on it. 
 
+                // only return the mines where the surrounding tile matches at least one of the tiles with a mine on it. 
                 return tilesWithMines.filter(mine => current === +mine) > 1;
+
                 /// filter through this array to only give true values (otherwise we get an array of true false true false etc..)
             }).filter(current => current);
 
-            // console.log(tilesWithMines);
-            console.log(mineMatches);
+            // if there is more than 0 mines in surrounding tiles, this number becomes the textcontent and the tile gets a class of tile-surrounding
             value = mineMatches.length > 0 ? mineMatches.length : "*";
 
             current.textContent = value;
 
             mineMatches.length > 0 ? current.classList.add("tile-surrounding") : null;
 
-            // value > 0 ? current.textContent = value : null;
-            });
-        } 
+        });
+    } 
 
-    createGrid(width, height);
-    addMines(number);
-    addNumberMines();
+    plotSurroundingTiles();
 
 })(document)
